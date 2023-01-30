bf3d1b3c7e4f02af25c12334e4b496d81b366fa4
evanFroelich <evanfroe1234@yahoo.com>
2023-01-30 07:36:52 -0800

    implemented question chains and reversible questions
    on correct question it now gives you another chosen at random.
    on incorrect question it tells you the answer and stops the quiz
    questions flagged as reversible can now show up reversed
    bot when in a question chain will now only respond to the user that started the quiz in the channel they started the quiz in

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from datetime import datetime
 
 
 class MyClient(discord.Client):
-    questionMode="open"
-    
+    questionMode='open'
+    curQuestion=''
+    curUser=''
+    curChannel=''
+    curFlipMode=0
     async def on_ready(self):
         
 
@@ -53,19 +56,80 @@ class MyClient(discord.Client):
         if len(localMessage)>0:
             splitString=localMessage.split('|')
 
-            if splitString[0]=='..quiz':
-                if self.questionMode=='open':
-                    self.questionMode='in-use'
-                    await message.channel.send(self.questionMode)
-                sqlStatment='''SELECT * FROM Questions ORDER BY RANDOM() LIMIT 1'''
-                tableRow=curs.execute(sqlStatment)
-                for row in tableRow:
-                    await message.channel.send(row)
+
+
+            if self.questionMode=='in-use':
+                if str(message.author.id) != self.curUser or str(message.channel.id) != self.curChannel:
+                    curs.close()
+                    conn.close()
+                    return
+                isCorrect='false'
+                if self.curFlipMode==0:
+
+                    if localMessage==self.curQuestion[3] or localMessage==self.curQuestion[4] or localMessage==self.curQuestion[5]:
+                        isCorrect='true'
+                else:
+                    if localMessage==self.curQuestion[0]:
+                        isCorrect='true'
+                if isCorrect=='true':
+                    await message.channel.send("correct")
+                    sqlStatment='''SELECT * FROM Questions ORDER BY RANDOM() LIMIT 1'''
+                    tableRow=curs.execute(sqlStatment)
+                    for row in tableRow:
+                        self.curQuestion=row
+                        if row[1]==1:
+                            r=random()
+                            print(r)
+                            if r<.5:
+                                self.curFlipMode=1
+                                await message.channel.send("translate: "+row[3])
+                            else:
+                                self.curFlipMode=0
+                                await message.channel.send("translate: "+row[0])
+                        else:
+                            self.curFlipMode=0
+                            await message.channel.send("translate: "+row[0])
+                else:
+                    await message.channel.send("incorrect. Correct answer is: "+ self.curQuestion[3] if self.curFlipMode==0 else "incorrect. Correct answer is: "+ self.curQuestion[0])
+                    self.questionMode='open'
                 curs.close()
                 conn.close()
                 return
+
+
+
+            if splitString[0]=='..quiz':
+                if self.questionMode=='open':
+                    self.questionMode='in-use'
+                    self.curChannel=str(message.channel.id)
+                    self.curUser=str(message.author.id)
+                    await message.channel.send(self.questionMode+" + "+self.curChannel+" + "+self.curUser)
+                sqlStatment='''SELECT * FROM Questions ORDER BY RANDOM() LIMIT 1'''
+                tableRow=curs.execute(sqlStatment)
+                for row in tableRow:
+                    self.curQuestion=row
+                    if row[1]==1:
+                        r=random()
+                        if r<.5:
+                            self.curFlipMode=1
+                            await message.channel.send("translate: "+row[3])
+                        else:
+                            self.curFlipMode=0
+                            await message.channel.send("translate: "+row[0])
+                    else:
+                        self.curFlipMode=0
+                        await message.channel.send("translate: "+row[0])
+                curs.close()
+                conn.close()
+                return
+
+
+
             if splitString[0]=='..format':
                 await message.channel.send("..add|question|reversable(1 or 0)|number of answers|answer1|answer2|answer3")
+            
+            
+            
             if splitString[0]=='..add':
                 #await message.channel.send("also not ready yet")
                 splitString[2]=int(splitString[2])
